5be9b13ece8e69348a43ab7391e80e4adf41e7f8
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2020-08-25 22:55:05 +0530

    complete setup

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config("./.env");
+}
+
 const express = require("express");
 const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+const indexRoute = require("./routes/index");
 
 const app = express();
+
 app.set("view engine", "ejs");
+app.set("views", `${__dirname}/views`);
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected To Mongoose"));
 
-const port = 8079;
+app.use("/", indexRoute);
+
+const port = process.env.PORT || 8079;
 app.listen(port, () => {
   console.log(`Server is running on port number ${port}`);
 });
